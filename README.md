1bbf156d77ecd03d4e984aba621f90c6d952e1fd
Oishik-Sinha <oishiksinha.india@gmail.com>
2025-06-11 21:15:24 +0530

    Final: Doing some refactor and adding helper function for better reusability! Update readme.md file for test

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # String Calculator TDD Kata (Python)
+
+This is an implementation of the String Calculator Kata using **Test-Driven Development (TDD)** in Python.
+
+## Features
+
+- Handles empty strings, 1 or many numbers
+- Supports `,` and `\n` as delimiters
+- Supports custom delimiters (e.g., `//;\n1;2`)
+- Throws exception on negative numbers
+
+## Requirements
+
+- Python 3.7+
+- `pytest`
+
+## Install & Run
+
+```bash
+git clone <your_repo_url>
+cd string_calculator
+pip install -r requirements.txt
+pytest -v
+```
+
+### Please Find The bellow result image of test runs
+![Test run result](https://github.com/Oishik-Sinha/calculator_tdd/blob/main/pytest_result.png)
--- string_calculator.py
+++ string_calculator.py
@@ -1,17 +1,40 @@
 import re
+from typing import List
 
 def add(numbers: str) -> int:
     if not numbers:
         return 0
     
-    delimiter = ",|\n"
-    if numbers.startswith("//"):
-        delim, numbers = numbers.split("\n", 1)
-        delimiter = delim[2:]
-
-    tokens = re.split(delimiter, numbers) 
-    nums = [int(t) for t in tokens]
-    neg = [n for n in nums if n < 0]
-    if neg:
-        raise ValueError("negative numbers not allowed " + ",".join(map(str, neg)))
+    delimiter, numbers_str = extract_delimiter_and_numbers(numbers)
+    tokens = split(numbers_str, delimiter)
+    nums =convert_to_list_of_int(tokens)
+    check_negative_values(nums)
     return sum(nums)  
+
+def extract_delimiter_and_numbers(string: str) -> tuple[str, str]:
+    """
+    Extract the numers and delimiters from the given string if any.\n
+    Example : The beginning of the string will contain a separate line that looks like this: "//[delimiter]\n[numbers…]". For example, "//;\n1;2" where the delimiter is ";" should return 3
+    """
+    if string.startswith("//"):
+        header, numbers = string.split("\n", 1)
+        custom_delimiter =  header[2:]
+        return custom_delimiter, numbers
+    return r",|\n", string
+
+
+def split(s: str, delimiter_pattern: str) -> List[str]:
+    """Split the number string using the given delimiter."""
+    return re.split(delimiter_pattern, s)
+
+
+def check_negative_values(numbers: list[int]) -> None:
+    """Raises ValueError if any negative numbers are found."""
+    negatives = [n for n in numbers if n < 0]
+    if negatives:
+        raise ValueError(f"negative numbers not allowed {','.join(map(str, negatives))}")
+    
+
+def convert_to_list_of_int(tokens: List[str]) -> List[int]:
+    "Convert the number string in list of integers"
+    return [int(t) for t in tokens if t.strip()]
